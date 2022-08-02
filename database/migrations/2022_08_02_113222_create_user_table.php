d0fda8f95d76042450af9fb1a79ded1914fd52a3
vannlp <phanvan0322@gmail.com>
2022-08-02 19:20:53 +0700

    update password comlum in user table

--- database/migrations/2022_08_02_113222_create_user_table.php
+++ database/migrations/2022_08_02_113222_create_user_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email');
+            $table->string('password');
             $table->string('username');
             $table->string('avatar');
             $table->string('address');
