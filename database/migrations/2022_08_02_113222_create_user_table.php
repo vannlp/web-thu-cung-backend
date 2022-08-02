5a57d1cbf5ba030b08ae51427e0417cab2f5277d
vannlp <phanvan0322@gmail.com>
2022-08-02 19:18:07 +0700

    create table by migration

---
+++ database/migrations/2022_08_02_113222_create_user_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('username');
+            $table->string('avatar');
+            $table->string('address');
+            $table->string('phone', 11);
+            $table->integer('active');
+            $table->foreignId('role_id')->references('id')->on('roles');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user');
+    }
+};
